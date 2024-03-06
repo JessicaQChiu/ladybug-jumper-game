785d2d11330b4910c157e66abd77a7a93eba445d
Jess <Jessica.Q.Chiu@gmail.com>
2024-03-05 20:05:31 -0800

    Update collision boxes
    Updating collision boxes/rectangles to adjust for the space the object art fills.

--- script.js
+++ script.js
@@ -48,10 +48,11 @@ function checkLose() {
     return getObstacleRects().some(rect => isCollision(rect, ladybugRect));
 };
 
+/*Collision boxes shifted to align with art*/
 function isCollision(rect1, rect2) {
     return (
-        rect1.left < rect2.right &&
-        rect1.top < rect2.bottom &&
+        rect1.left < (rect2.right - 15) &&
+        rect1.top <(rect2.bottom - 15)&&
         rect1.right > rect2.left &&
         rect1.bottom > rect2.top
     );
